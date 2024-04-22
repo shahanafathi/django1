3af86e9ac9661d7a6c43a4fae83931fb5484746e
ismail1712 <muhammedismailp12@gmail.com>
2024-04-22 15:41:49 +0530

    first

--- pjt1/app1/views.py
+++ pjt1/app1/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http.response import HttpResponse
+from .models import regiter,rstr
+
 # Create your views here.
 def sr (request):
    return HttpResponse("name")
-
-
 
 def odd (request,num):
 
@@ -24,13 +24,11 @@ def abc(request,n,m):
       c =m+n
       return HttpResponse(c)
   
-  
 def index(request):
     return render(request,'index.html')
 
 def ind(request):
     return render(request,'index1.html')
-
 
 def vowel(request, v):
     vowels = ["a", "e", "i", "o", "u"]
@@ -40,4 +38,60 @@ def vowel(request, v):
         return HttpResponse("%s is not a vowel" % v)
     
 def indo(request):
-    return render(request,'index3.html')
+    if request.method == 'POST':
+        frst=request.POST['fname']
+        lst=request.POST['lname']
+        phn=request.POST['phone_number']  
+        email=request.POST['email']   
+        password=request.POST['psw'] 
+        data=regiter.objects.create(firstname=frst,lastname=lst,phonenumber=phn,email=email,password=password)
+        data.save()     
+        return HttpResponse('sucessfull')
+    else :
+        return render(request,'index3.html')
+    
+    
+def ind_4(request):
+    if request.method == 'POST':
+        sclnm=request.POST['sname']
+        dst=request.POST['Dict']
+        adrss=request.POST['adrs']
+        phone=request.POST['phn_no']
+        email=request.POST['email']
+        pswd=request.POST['psw']
+        data =rstr.objects.create(schlname=sclnm,district=dst,address=adrss,phoneno=phone,email=email, password=pswd)
+        data.save()
+        return redirect(rgr)
+    else :
+        return render(request,'index4.html')
+    
+def rgr(request):
+    a=rstr.objects.all()
+    return render(request,'indexl.html',{'abc':a})
+    
+def search(request):
+    if request.method == 'POST':
+        data = request.POST['schoolname']
+        a = rstr.objects.filter(schlname=data)
+        return render(request,'indexl.html',{'abc':a})
+    else:
+            return render(request,'indexl.html')
+        
+def deleteschool(request,id):
+    da = rstr.objects.get(id=id)
+    da.delete()
+    return redirect(rgr)
+
+def editschool(request,id):
+    di= rstr.objects.get(id=id)
+    if request.method =='POST':
+        di.schlname=request.POST['sname']
+        di.district=request.POST['Dict']
+        di.address=request.POST['adrs']
+        di.phoneno=request.POST['phn_no']
+        di.email=request.POST['email']
+        di.password=request.POST['psw']
+        di.save()
+        return redirect(rgr)
+    else:
+        return render(request,'index5.html',{'data':di})
--- pjt1/pjt1/urls.py
+++ pjt1/pjt1/urls.py
@@ -17,16 +17,27 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app1 import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/',admin.site.urls),
     path('sr',views.sr),
     path('odd/<int:num>',views.odd),
     path('str/<n>',views.str),
     path('abc/<int:n>/<int:m>',views.abc),
     path('',views.index),
     path('ind',views.ind,name='ind'),
-    path('vowel/<v>',views.vowel),
-     path('indo',views.indo,name='indo'),
-     
+    path('vowel/<str:v>',views.vowel),
+    path('indo',views.indo,name='indo'),
+    path('ind_4',views.ind_4,name='ind_4'),
+    path('rgr',views.rgr,name='rgr'),
+    path('search',views.search,name='search'),
+    path('deleteschool/<int:id>',views.deleteschool,name='deleteschool'),
+    path('editschool/<int:id>',views.editschool,name='editschool'),
+    
+    
 ]
+if settings.DEBUG:
+    
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
